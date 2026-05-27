2e6dd678765db4ec8b73a118479a0316bee2a07c
anastasijas2101 <anastasijas101@gmail.com>
2026-05-27 10:14:07 +0200

    Project completed

---
+++ backend/controllers/product.controller.js
@@ -0,0 +1,145 @@
+import Product from '../models/product.model.js';
+import { redis } from '../lib/redis.js';
+import imagekit from '../lib/imagekit.js';
+
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.json({ products });
+  } catch (error) {
+    console.log('Error in getAllProducts controller', error.message);
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+};
+
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    let featuredProducts = await redis.get('featured_products');
+    if (featuredProducts) {
+      return res.json(JSON.parse(featuredProducts));
+    }
+
+    featuredProducts = await Product.find({ isFeatured: true }).lean();
+
+    if (!featuredProducts) {
+      return res.status(404).json({ message: 'No featured products found' });
+    }
+
+    await redis.set('featured_products', JSON.stringify(featuredProducts));
+
+    res.json(featuredProducts);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+};
+
+export const createProduct = async (req, res) => {
+  try {
+    const { name, description, price, image, category } = req.body;
+
+    let imagekitResponse = null;
+
+    if (image) {
+      imagekitResponse = await imagekit.upload({
+        file: image,
+        fileName: `${Date.now()}.png`,
+        folder: '/products',
+      });
+    }
+
+    const product = await Product.create({
+      name,
+      description,
+      price,
+      image: imagekitResponse?.url ? imagekitResponse.url : '',
+      imageFileId: imagekitResponse?.fileId ? imagekitResponse.fileId : '',
+      category,
+    });
+
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    if (product.imageFileId) {
+      try {
+        await imagekit.deleteFile(product.imageFileId);
+      } catch (error) {
+        console.log('Error deleting image from ImageKit', error.message);
+      }
+    }
+
+    await Product.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Product deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+};
+
+export const getRecommendedProducts = async (req, res) => {
+  try {
+    const products = await Product.aggregate([
+      {
+        $sample: { size: 3 },
+      },
+      {
+        $project: {
+          _id: 1,
+          name: 1,
+          description: 1,
+          image: 1,
+          price: 1,
+        },
+      },
+    ]);
+
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+};
+
+export const getProductByCategory = async (req, res) => {
+  const { category } = req.params;
+  try {
+    const products = await Product.find({ category });
+    res.json({ products });
+  } catch (error) {
+    res.status(500).json({ message: 'Server errpr', error: error.message });
+  }
+};
+
+export const toggleFeaturedProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      product.isFeatured = !product.isFeatured;
+      const updateProduct = await product.save();
+      await updateFeaturedProductsCache();
+
+      res.json(updateProduct);
+    } else {
+      res.status(404).json({ message: 'Product not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+};
+
+async function updateFeaturedProductsCache() {
+  try {
+    const featuredProducts = await Product.find({ isFeatured: true }).lean();
+    await redis.set('featured_products', JSON.stringify(featuredProducts));
+  } catch (error) {
+    console.log('error in update cache function');
+  }
+}
